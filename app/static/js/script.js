c46bf0fe9705f11a0f9e44a6c1cc11186ec2b1b7
Koos <>
2018-02-09 12:20:52 +0100

    Added handleEvents function, preventdefault on anchors, fixed indenting

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,32 +1,41 @@
 (function () {
-
-    const app = {
-      init: function () {
-        routes.init()
-        blocks.toggle(location.hash)
-      }
-    }
-
-    const routes = {
-      init: function () {
-            window.addEventListener('hashchange', () => {
-            blocks.toggle(location.hash)
+  const app = {
+    init: function () {
+      routes.init()
+      blocks.init()
+      this.handleEvents()
+    },
+    handleEvents: function () {
+      document.querySelectorAll('nav a').forEach((link) => {
+        link.addEventListener('click', (e) => {
+          e.preventDefault() // prevent jump
+          blocks.toggle(e.target.hash)
+          window.location.hash = e.target.hash // set url to match hash
         })
-      }
+      })
     }
+  }
 
-    const blocks = {
-      toggle: function (route) {
-        const sections = document.querySelectorAll('section')
-        let active = document.querySelector(route)
-        
-        sections.forEach(function(section) {
-            section.classList.remove('active')
-        })
-        active.classList.add('active')
-      }
+  const routes = {
+    init: function () {
+      window.addEventListener('hashchange', () => {
+        blocks.toggle(window.location.hash)
+      })
     }
+  }
 
-    app.init()
-    
-  })()
+  const blocks = {
+    init: function () {
+      blocks.toggle(window.location.hash)
+    },
+    toggle: function (route) {
+      const sections = document.querySelectorAll('section')
+      let active = document.querySelector(route)
+      sections.forEach(function (section) {
+        section.classList.remove('active')
+      })
+      active.classList.add('active')
+    }
+  }
+  app.init()
+})()
